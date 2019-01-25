4cd2230610bb7ff115e6b6ead1c65a2ebeae5676
Paxnamra <bialyfeniks4@gmail.com>
2019-01-25 16:59:16 +0100

    TDD approach | designing tests

---
+++ src/test/java/GradeBook.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+public class GradeBook {
+
+    @Test
+    public void shouldCreateGradeBook(){
+
+    }
+
+    @Test
+    public void shouldAddSubjectToGradeBook(){
+
+    }
+
+    @Test
+    public void shouldGetSubjectFromGradeBook(){
+
+    }
+
+    @Test
+    public void shouldCalculateGradeBookAverage() {
+
+    }
+}
---
+++ src/test/java/Subject.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+public class Subject {
+
+    @Test
+    public void shouldCreateSubject(){
+
+    }
+
+    @Test
+    public void shouldAddSubject() {
+
+    }
+
+    @Test
+    public void shouldAddMarkToSubject() {
+
+    }
+
+    @Test
+    public void shouldCalculateSubjectAverage() {
+
+    }
+}
